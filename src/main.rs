f7265a549638d7486c04dead46f08d1f1b7b8232
Dievo Villegas Arenas <diego.villegas.12@sansano.usm.cl>
2024-02-17 18:37:37 -0300

    Se computa la cantidad de días de vacaciones que el usuario tiene disponible hasta la fecha.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::fs::File;
+use std::io::{BufWriter, Write, Read};
+use serde::{Deserialize, Serialize};
+use chrono::{Utc, NaiveDate};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Empleado {
+    nombre: String,
+    fecha_ingreso: NaiveDate,
+    dias_utilizados: u64,
+}
+
+fn read_employ(path: &str) -> Empleado {
+    let mut file = File::open(path).expect(format!("No se pudo encontrar el archivo {path}.").as_str());
+    let mut data = String::new();
+    file.read_to_string(&mut data).expect(format!("No se pudo leer del archivo {path}").as_str());
+
+    let employ: Empleado = serde_json::from_str(&data).expect(format!("{path} no tiene el formato esperado").as_str());
+
+    return employ;
+}
+
+fn write_employ(path: &str, employ: &Empleado) {
+    let file = File::create(path).expect(format!("No se pudo crear el archivo {path}.").as_str());
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer(&mut writer, employ).expect(format!("Error al preparar el writer del archivo {path}").as_str());
+    writer.flush().expect(format!("Error al escribir en el archivo {path}").as_str());
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "docs/data.json";
+    let employ: Empleado = read_employ(filename);
+
+    println!("{:?}", employ);
+
+    let current_date = Utc::now().naive_utc().date();
+    let diff = current_date.signed_duration_since(employ.fecha_ingreso);
+    let days = diff.num_days();
+    let vacation_days_total: f64 = days as f64*0.0411;
+    let vacation_days_available: f64 = vacation_days_total - employ.dias_utilizados as f64;
+
+    println!("Días de vacaciones disponibles a la fecha {}: {}", current_date, vacation_days_available);
+
+    write_employ(filename, &employ)
 }
